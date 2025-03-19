1fab8feecbbdef36b82ba97c4b6d3daa28cb78fa
HabibUjjan <151654995+HabibUjjan@users.noreply.github.com>
2025-03-20 00:59:55 +0500

    This is the SMS Project

---
+++ PROJECTS (OOPS)/Student_Managment_System/src/com/Student/SMS/Test.java
@@ -0,0 +1,10 @@
+package com.Student.SMS;
+
+public class Test {
+        public static void main(String[] args) {
+            Students_Management_System studentsManagementSystem = new Students_Management_System();
+            studentsManagementSystem.showMenu();
+        }
+    }
+
+
