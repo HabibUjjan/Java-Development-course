1d1e22993e2cfac1f5444fff8ed4ff94d44f1525
Habibullah Ujjan <151654995+HabibUjjan@users.noreply.github.com>
2025-05-08 02:36:15 +0500

    Delete JAVA_8_CONCEPTS_PROGRAMS/src/com/Studennt/STREAM_API_PROJECTS directory

--- JAVA_8_CONCEPTS_PROGRAMS/src/com/Studennt/STREAM_API_PROJECTS/J.java
+++
@@ -1,4 +0,0 @@
-package com.Studennt.STREAM_API_PROJECTS;
-
-public class J {
-}
--- JAVA_8_CONCEPTS_PROGRAMS/src/com/Studennt/STREAM_API_PROJECTS/SMS/Test.java
+++
@@ -1,8 +0,0 @@
-package com.Studennt.STREAM_API_PROJECTS.SMS;
-
-public class Test {
-    public static void main(String[] args) {
-        Students_Management_System studentsManagementSystem = new Students_Management_System();
-        studentsManagementSystem.showMenu();
-    }
-}
