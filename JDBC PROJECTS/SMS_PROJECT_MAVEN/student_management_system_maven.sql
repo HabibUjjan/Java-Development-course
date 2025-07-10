c9d350000716035ba0b00768f61bca9f947b5363
HabibUjjan <151654995+HabibUjjan@users.noreply.github.com>
2025-07-11 00:07:29 +0500

    Create student_management_system_maven.sql

---
+++ JDBC PROJECTS/SMS_PROJECT_MAVEN/student_management_system_maven.sql
@@ -0,0 +1,18 @@
+create database student_management_system_maven;
+
+use student_management_system_maven;
+
+create table department (
+    d_name varchar(100),
+    d_code varchar(50) primary key
+);
+
+create table student (
+    s_name varchar(100),
+    s_rollno varchar(50) primary key,
+    s_age int,
+    s_dept varchar(50),
+    foreign key (s_dept) references department(d_code)
+);
+
+select * from student;
